6f1909f7fc41e2d9cf6d9d1ac6d990f03b233b16
ilepsky <ilepsky@yandex.ru>
2026-05-27 23:06:39 +0300

    rabver

--- tileshop/settings.py
+++ tileshop/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8x!q&5^7@9z#1m2n3b4v5c6x7z8a9s0d1f2g3h4j5k6l7'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
